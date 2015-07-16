52aa19f0d6c7206579786e7093a8318318fdb64f
丹澤智也 <TTA2161@gmail.com>
2015-07-16 10:05:56 +0900

    OPTION画面完成

---
+++ SoundRider/Assets/Resources/Scripts/HomeScript/SoundManagerScript/SoundManager.cs
@@ -0,0 +1,27 @@
+﻿// ◆ Date 2015.07.08 wed. Producer : Tomoya Tanzawa
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+// ● SoundManagerClass
+public class SoundManager : MonoBehaviour
+{
+    // ▼ Object that music is played
+    public GameObject TargetBGMSoundObject;
+	public GameObject TargetEFFECTSoundObject;
+    // ▼ BGMSliderObject
+	public GameObject TargetBGMSlider;
+	// ▼ EFFECTSliderObject
+	public GameObject TargetEFFECTSlider;
+    // ▼ Use this for initialization
+    void Start()
+    {
+    }
+    // ▼ Update is called once per frame
+    void Update()
+    {
+		// ▼ Get the first to touch the location
+		TargetBGMSoundObject.GetComponent<AudioSource> ().volume = (float)(TargetBGMSlider.GetComponent<Slider> ().value / 10);
+		TargetEFFECTSoundObject.GetComponent<AudioSource> ().volume = (float)(TargetEFFECTSlider.GetComponent<Slider> ().value / 10);
+	}
+}
---
+++ SoundRider/Assets/Resources/Scripts/HomeScript/SoundManagerScript/SoundManagerReadMe.txt
@@ -0,0 +1,20 @@
+サウンドマネージャーの使い方を説明させて頂きます。
+I will explain how to use the Sound Manager.
+
+①BGMがついているObjectを、TargetBGMSoundObjectに設定します。(GameObject)
+  An Object that BGM is on, set to TargetBGMSoundObject. (GameObject)
+
+②BGMを設定するSliderをTargetBGMSliderに設定します。(Slider)
+  You set the Slider to set the BGM to TargetBGMSlider. (Slider)
+
+③SoundEffectがついているObjectを、TargetEffectSoundObjectに設定します。(GameObject)
+  An Object that SoundEffec is on, set to TargetEffectSoundObject. (GameObject)
+
+④SoundEffecを設定するSliderをTargetEffectSliderに設定します。(Slider)
+  You set the Slider to set the SoundEffec to TargetEffectSlider. (Slider)
+
+⑤設定完了です。
+  Setup is complete.
+
+分からない部分があれば、丹澤までお願いします。
+If you do not know part, thank you to the Tanzawa.
